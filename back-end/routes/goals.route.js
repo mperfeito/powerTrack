18ec4231627d454f2c808295eaee940d485a2e03
mperfeito <mperfeito.08@gmail.com>
2025-06-13 16:26:58 +0100

    change progress route

--- back-end/routes/goals.route.js
+++ back-end/routes/goals.route.js
@@ -5,10 +5,12 @@ import * as controller from "../controllers/goals.controller.js";
 const router = express.Router();
 
 router.get("/", controller.getAllGoals); // GET /api/goals ☑️
+                                        // GET /api/goals?includeProgress=true
+                                        
 router.get("/:id_goal", controller.getGoalById); // GEt /api/goals/{id} ☑️ 
 router.patch("/:id_goal", controller.updateGoal); // PATCH /api/goals/{id} ☑️
 
-router.get("/:id_goal/calculate-progress", controller.calculateGoalProgress); //GET /api/goals/{id}/calculate-progress
+
 
 router.post("/", controller.addGoal); // POST /api/goals ☑️
 router.delete("/:id_goal", controller.deleteGoal); // DELETE /api/goals/{id} ☑️
